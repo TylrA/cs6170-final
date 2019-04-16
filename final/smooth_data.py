3baee592918e439392a80d280ac015bafccd6171
morsgiathatch <corbinwilliambaldwin@gmail.com>
2019-04-16 12:44:24 -0600

    Update bash script, begin data sanitation preparation

---
+++ final/smooth_data.py
@@ -0,0 +1,46 @@
+import numpy as np
+import sys
+import os
+
+
+def denoise_using_reg_ls(sample, lambd):
+    dim = sample.shape[0]
+    iden = np.identity(dim)
+    L = np.zeros((dim - 1, dim))
+    for i in range(0, dim - 1):
+        L[i, i] = 1
+        L[i, i + 1] = -1
+
+    return np.linalg.inv(iden + lambd * (L.transpose()).dot(L)).dot(sample)
+
+
+# Main takes 1 command line argument, a float 'lambd' that corresponds to the hyperparameter
+# of regularized least squares. Suggested range of lambd is [0.0, 100]. 0.0 corresponds to
+# no smoothing and the larger the value of lambd the more smooth the regularization. Keep in
+# mind that too high of a value for 'lambd' will lead to decreased accuracy of the interpolated
+# data.
+if __name__ == '__main__':
+    lambd = float(sys.argv[1])
+    cwd = os.getcwd()
+    parent = os.path.join(cwd, os.path.join(os.path.dirname(__file__)))
+    boundary_file_names = os.listdir(parent + "/boundary_points")
+    for file_name in boundary_file_names:
+        x = []
+        with open(parent + "/boundary_points/" + file_name, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                terms = line.split(',')
+                point = []
+                for term in terms:
+                    point.append(float(term))
+                x.append(point)
+
+        regularized_x = denoise_using_reg_ls(np.array(x), lambd)
+        with open(parent + "/smoothed_boundary_points/" + file_name, 'w') as f:
+            print_x = regularized_x.tolist()
+            for point in print_x:
+                for i, term in enumerate(point):
+                    if i == len(point) - 1:
+                        print(str(term), f)
+                    else:
+                        print(str(term) + ',', f)
--- simulation/run.sh
+++ simulation/run.sh
@@ -14,21 +14,27 @@ else
 fi
 cd ./gifs
 
-for i in 1 2 3 4 5 6 6 8 9 10
+for i in 0 1 2 3 4 5 6 7 8 9
 do
     rm -rf "./gifs$i"
     mkdir "./gifs$i"
+    cd "./gifs$i"
+    for j in 0 1 2 3 4 5 6 7 8 9
+    do
+	mkdir "./sim$j"
+    done
+    cd ../
 done
 
 rho1s=(1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9)
 rho0s=(1.0 0.9 0.8 0.7 0.6 0.5 0.4 0.3 0.2 0.1)
 
 cd ../palabos-v2.0r0/examples/showCases/multiComponent2d
-for i in 1 2 3 4 5 6 7 8 9 10
+for i in 0 1 2 3 4 5 6 7 8 9
 do
     for j in 0 1 2 3 4 5 6 7 8 9
     do
-        if ! ./rayleighTaylor2D 160000 ${rho1s[j]} ${rho0s[j]} "/gifs$i"
+        if ! ./rayleighTaylor2D 160000 ${rho1s[j]} ${rho0s[j]} "./gifs$i/sim$j"
         then
             printf "An error occured generating simulation\n"
             cd ../../../../
