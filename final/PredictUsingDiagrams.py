2580ea82badebb2f2665029c39e0d60c41d48da1
morsgiathatch <corbinwilliambaldwin@gmail.com>
2019-04-22 16:28:29 -0600

    Update to print to file

--- final/PredictUsingDiagrams.py
+++ final/PredictUsingDiagrams.py
@@ -5,6 +5,18 @@ from sklearn.svm import SVC
 import numpy as np
 # from scipy.interpolate import splprep, splev
 import sys
+import random
+
+
+def print_diagram_to_file(gifs, sim, diagram):
+    cwd = os.getcwd()
+    parent = os.path.join(cwd, os.path.join(os.path.dirname(__file__)))
+    print_path = parent + "/diagrams/"
+
+    with open (print_path + "gifs%i-sim%i.txt" % (gifs, sim), 'w') as f:
+        for entry in diagram:
+            print("[%d,%d)" % (float(entry[0]), float(entry[1])), file=f)
+
 
 
 def get_critical_points_from_file(path, time):
@@ -21,6 +33,7 @@ def get_critical_points_from_file(path, time):
     # Interpolate to non-functional spli)ne
     x_coords = np.array(x_coords)
     y_coords = np.array(y_coords)
+    # Interpolation is currently broken. If fixing is desired, code remains commented
     # tck, u= splprep(x=[x_coords, y_coords],  w=None)
     # new_points = splev(u, tck)
     # x_coords = new_points[0]
@@ -48,7 +61,7 @@ def get_critical_points_from_file(path, time):
     return critical_points
 
 
-def build_persistence_diagram_from_data(path_to_files, curr_count, max_count):
+def build_persistence_diagram_from_data(path_to_files, curr_count, max_count, gifs):
     point_files = os.listdir(path_to_files)
     point_files.sort()
     graphs = []
@@ -77,6 +90,7 @@ def build_persistence_diagram_from_data(path_to_files, curr_count, max_count):
 
     diagram = critical_point_graph.PersistenceDiagram(graphs, len(point_files))
     diagram.generate_diagram()
+    print_diagram_to_file(gifs, curr_count, diagram.diagram)
 
     # Use the number of data files per simulation as the 'death time'
     return np.array(diagram.diagram)
@@ -92,20 +106,38 @@ def generate_diagrams(num_classes_, num_samples_):
     train_diagrams_ = []
     test_labels_ = []
     test_diagrams_ = []
+    labels_ = []
+    diagrams_ = []
     for i_ in range(0, num_classes_):
         print("\nBuilding diagrams for class %i / %i" % (i_, num_classes_))
         label_abs_dir = main_points_dir + "/" + dirs_by_label[i_]
         simulation_dirs = os.listdir(label_abs_dir)
         simulation_dirs.sort()
 
-        for j in range(0, num_samples_):
-            train_labels_.append(dirs_by_label[i_])
-            train_diagram = build_persistence_diagram_from_data(label_abs_dir + "/" + simulation_dirs[j], j, len(simulation_dirs))
-            train_diagrams_.append(train_diagram)
-        for j in range(num_samples_, len(simulation_dirs)):
-            test_labels_.append(dirs_by_label[i_])
-            test_diagram = build_persistence_diagram_from_data(label_abs_dir + "/" + simulation_dirs[j], j, len(simulation_dirs))
-            test_diagrams_.append(test_diagram)
+        # for j in range(0, num_samples_):
+        #     train_labels_.append(dirs_by_label[i_])
+        #     train_diagram = build_persistence_diagram_from_data(label_abs_dir + "/" + simulation_dirs[j], j, len(simulation_dirs))
+        #     train_diagrams_.append(train_diagram)
+        # for j in range(num_samples_, len(simulation_dirs)):
+        #     test_labels_.append(dirs_by_label[i_])
+        #     test_diagram = build_persistence_diagram_from_data(label_abs_dir + "/" + simulation_dirs[j], j, len(simulation_dirs))
+        #     test_diagrams_.append(test_diagram)
+
+        for j in range(0, len(simulation_dirs)):
+            labels_.append(dirs_by_label[i_])
+            train_diagram = build_persistence_diagram_from_data(label_abs_dir + "/" + simulation_dirs[j], j, len(simulation_dirs), gifs=i_)
+            diagrams_.append(train_diagram)
+
+        sample_indices = random.sample(range(0, len(simulation_dirs)), num_samples)
+        sample_indices.sort()
+
+        for j in range(0, len(simulation_dirs)):
+            if j in sample_indices:
+                train_diagrams_.append(diagrams_[j])
+                train_labels_.append(labels_[j])
+            else:
+                test_diagrams_.append(diagrams_[j])
+                test_labels_.append(labels_[j])
 
     return train_labels_, train_diagrams_, test_labels_, test_diagrams_
 
@@ -133,195 +165,38 @@ def run_tests(bandwidths_list, kernel, training_labels_, training_diagrams_, tes
 
 
 if __name__ == '__main__':
-    num_classes = int(input("How many image classes would you like to use? 2 or 4\n"))
-    num_samples = int(input("How many samples per class would you like? 0 - 10\n"))
-    training_labels, training_barcodes, test_labels, test_barcodes = generate_diagrams(num_classes, num_samples)
+    choice = input("Would you like to use Ripser? If no, then the critical point evolution predictor will be run. y/n")
+    if choice == 'n':
+        num_classes = int(input("How many image classes would you like to use? 2 or 4\n"))
+        num_samples = int(input("How many samples per class would you like? 0 - 10\n"))
+        training_labels, training_barcodes, test_labels, test_barcodes = generate_diagrams(num_classes, num_samples)
 
-    training_diagrams = []
-    for i in range(0, len(training_barcodes)):
-        training_diagrams.append(np.array(training_barcodes[i]))
+        training_diagrams = []
+        for i in range(0, len(training_barcodes)):
+            training_diagrams.append(np.array(training_barcodes[i]))
 
-    testing_diagrams = []
-    for i in range(0, len(test_barcodes)):
-        testing_diagrams.append(np.array(test_barcodes[i]))
+        testing_diagrams = []
+        for i in range(0, len(test_barcodes)):
+            testing_diagrams.append(np.array(test_barcodes[i]))
 
-    bandwidths = [0.5, 0.7, 0.9, 1.1, 1.3, 1.5]
+        bandwidths = [0.5, 0.7, 0.9, 1.1, 1.3, 1.5]
 
-    cwd = os.getcwd()
-    parent = os.path.join(cwd, os.path.join(os.path.dirname(__file__)))
-    with open(parent + "/diagram_training_results.txt", 'w') as f:
+        cwd = os.getcwd()
+        parent = os.path.join(cwd, os.path.join(os.path.dirname(__file__)))
+        with open(parent + "/diagram_training_results.txt", 'w') as f:
 
-        # Default Kernel
-        print("\nUsing default RBF Kernel\n", file=f)
-        run_tests(bandwidths, None, training_labels, training_barcodes, test_labels, test_barcodes, f)
+            # Default Kernel
+            print("\nUsing default RBF Kernel\n", file=f)
+            run_tests(bandwidths, None, training_labels, training_barcodes, test_labels, test_barcodes, f)
 
-        # Linear Kernel
-        print("\nUsing Linear Kernel\n", file=f)
-        run_tests(bandwidths, 'linear', training_labels, training_barcodes, test_labels, test_barcodes, f)
+            # Linear Kernel
+            print("\nUsing Linear Kernel\n", file=f)
+            run_tests(bandwidths, 'linear', training_labels, training_barcodes, test_labels, test_barcodes, f)
 
-        # Polynomial Kernel
-        print("\nUsing Cubic Polynomial Kernel\n", file=f)
-        run_tests(bandwidths, 'poly', training_labels, training_barcodes, test_labels, test_barcodes, f)
+            # Polynomial Kernel
+            print("\nUsing Cubic Polynomial Kernel\n", file=f)
+            run_tests(bandwidths, 'poly', training_labels, training_barcodes, test_labels, test_barcodes, f)
 
-        # Sigmoid Kernel
-        print("\nUsing Sigmoid Kernel\n", file=f)
-        run_tests(bandwidths, 'sigmoid', training_labels, training_barcodes, test_labels, test_barcodes, f)
-
-
-
-def get_diagrams(num_classes, num_training_examples_per_class):
-    cwd = os.getcwd()
-    parent = os.path.join(cwd, os.path.join(os.path.dirname(__file__)))
-    dim_0_barcode_dirs = os.listdir(parent + "/../Data/ImageBarcodes/dim0/")
-    dim_0_barcode_dirs.sort()
-    dim_1_barcode_dirs = os.listdir(parent + "/../Data/ImageBarcodes/dim1/")
-    dim_1_barcode_dirs.sort()
-    labels = []
-    for im_dir in dim_0_barcode_dirs:
-        labels.append(im_dir.split("-")[0])
-
-    barcodes = []
-    dim_0_barcodes = []
-    dim_1_barcodes = []
-    # min_dim_0 = float('inf')
-    # min_dim_1 = float('inf')
-    # for bar_dir in dim_0_barcode_dirs:
-    #     with open(parent + "/../Data/ImageBarcodes/dim0/" + bar_dir, 'r') as f:
-    #         lines = f.readlines()
-    #         if len(lines) < min_dim_0:
-    #             min_dim_0 = len(lines)
-    # for bar_dir in dim_1_barcode_dirs:
-    #     with open(parent + "/../Data/ImageBarcodes/dim1/" + bar_dir, 'r') as f:
-    #         lines = f.readlines()
-    #         if len(lines) < min_dim_1:
-    #             min_dim_1 = len(lines)
-
-    for bar_dir in dim_0_barcode_dirs:
-        im_bars = []
-        with open(parent + "/../Data/ImageBarcodes/dim0/" + bar_dir, 'r') as f:
-            lines = f.readlines()
-            for line in lines:
-                terms = (line.strip()).split(" ")
-                point = [float(terms[0]), float(terms[1])]
-                im_bars.append(point)
-        # im_bars = random.sample(im_bars, min_dim_0)
-        dim_0_barcodes.append(im_bars)
-
-    for bar_dir in dim_1_barcode_dirs:
-        with open(parent + "/../Data/ImageBarcodes/dim1/" + bar_dir, 'r') as f:
-            im_bars = []
-            lines = f.readlines()
-            for line in lines:
-                terms = (line.strip()).split(" ")
-                point = [float(terms[0]), float(terms[1])]
-                im_bars.append(point)
-
-        # im_bars = random.sample(im_bars, min_dim_1)
-        dim_1_barcodes.append(im_bars)
-
-    for i in range(0, len(dim_0_barcodes)):
-        barcodes.append(dim_0_barcodes[i] + dim_1_barcodes[i])
-
-    training_barcodes = []
-    training_labels = []
-    training_indices = []
-    for i in range(0, num_classes):
-        indices = random.sample(list(range(10 * i, (10 * i + 10))), num_training_examples_per_class)
-        indices.sort()
-        training_indices += indices
-        for index in indices:
-            training_barcodes.append(barcodes[index])
-            training_labels.append(labels[index])
-
-    test_barcodes = []
-    test_labels = []
-    for i in range(0, 10 * num_classes):
-        if training_indices.count(i) == 0:
-            test_barcodes.append(barcodes[i])
-            test_labels.append(labels[i])
-
-    return training_labels, training_barcodes, test_labels, test_barcodes
-
-
-def construct_barcodes(parent, image_boundaries_path, barcodes_path):
-
-    image_boundaries_dirs_temp = os.listdir(image_boundaries_path)
-    image_boundaries_dirs = []
-    for im_dir in image_boundaries_dirs_temp:
-        image_boundaries_dirs.append(image_boundaries_path + im_dir)
-
-    image_names = []
-    for im_dir in image_boundaries_dirs_temp:
-        image_names.append(im_dir.replace(".txt", ""))
-
-    print("Constructing Barcodes")
-    sys.stdout.write('Progress: [%s]' % (' ' * 80))
-    sys.stdout.flush()
-    for i, im_dir in enumerate(image_boundaries_dirs):
-        process = Popen([parent + "/../ripser/ripser", im_dir, "--format", "point-cloud"], stdout=PIPE)
-        (output, err) = process.communicate()
-        exit_code = process.wait()
-
-        lines = (output.decode("utf-8")).split('\n')
-        dim0lines = []
-        dim1lines = []
-        flag0 = False
-        flag1 = False
-        for line in lines:
-            test_line = line.lstrip(' ')
-            if line == "":
-                continue
-            elif test_line == "persistence intervals in dim 0:":
-                flag0 = True
-                continue
-            elif test_line == "persistence intervals in dim 1:":
-                flag0 = False
-                flag1 = True
-                continue
-
-            elif flag0 or flag1:
-                l_trim = test_line.lstrip(" [-")
-                r_trim = l_trim.rstrip(" -)\n")
-                trimmed_line = r_trim.replace(",", " ")
-
-                # Remove barcode with infinite persistence
-                if flag0 and len(trimmed_line.strip().split(" ")) > 1:
-                    if trimmed_line.strip().split(" ")[1] != "":
-                        dim0lines.append(trimmed_line)
-                else:
-                    dim1lines.append(trimmed_line)
-
-        # Remove duplicate entries
-        dim0lines = list(set(dim0lines))
-        dim1lines = list(set(dim1lines))
-
-        with open(barcodes_path + "dim0/" + image_names[i] + "dim0.txt", 'w') as f:
-            for line in dim0lines:
-                print(line, file=f)
-
-        with open(barcodes_path + "dim1/" + image_names[i] + "dim1.txt", 'w') as f:
-            for line in dim1lines:
-                print(line, file=f)
-
-        sys.stdout.write('\rProgress: [%s' % ('#' * (i + 1)))
-        sys.stdout.write('%s]' % (' ' * (80 - (i + 1))))
-        sys.stdout.flush()
-
-    # For some reason the dim1 lines print differently. Perhaps this is a bug in ripser?
-    image_boundaries_dirs_temp = os.listdir(parent + "/../Data/ImageBarcodes/dim1/")
-    image_boundaries_dirs = []
-    for im_dir in image_boundaries_dirs_temp:
-        image_boundaries_dirs.append(parent + "/../Data/ImageBarcodes/dim1/" + im_dir)
-    for im_dir in image_boundaries_dirs:
-        lines = []
-        with open(im_dir, "r") as f:
-            content = f.readlines()
-            for i, line in enumerate(content):
-                trim_line = line.rstrip("\n")
-                temp_line = trim_line.split(" ")
-                if trim_line.split(" ")[1] != "":
-                    lines.append(trim_line)
-
-        with open(im_dir, "w") as f:
-            for line in lines:
-                print(line, file=f)
+            # Sigmoid Kernel
+            print("\nUsing Sigmoid Kernel\n", file=f)
+            run_tests(bandwidths, 'sigmoid', training_labels, training_barcodes, test_labels, test_barcodes, f)
